1de2efe16ca2c5a1e55740dc934f05c1ad893711
nnnlog <sorisem4106@naver.com>
2021-03-09 17:37:13 +0900

    cors 추가

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ auth.init();
 const db = require('./lib/db');
 db.connect();
 
+const cors = require("cors");
+app.use(cors());
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
